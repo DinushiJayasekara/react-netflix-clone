5d0e445cc21fbb126498912059ab55f2ec692ec8
DinushiJayasekara <jkdrj02@gmail.com>
2020-10-24 17:45:40 +0530

    style-banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from '../../axios';
 import requests from '../../requests';
 
+import './Banner.css'
+
 function Banner() {
 
     const [movie, setMovie] = useState([]);
@@ -19,7 +21,9 @@ function Banner() {
         fetchData();
     }, []);
 
-    console.log(movie);
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
 
     return (
         <header className="banner"
@@ -33,7 +37,9 @@ function Banner() {
             <div className="banner__contents">
 
                 {/* Movie Title */}
-                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
 
                 {/* Buttons */}
                 <div className="banner__buttons">
@@ -43,8 +49,10 @@ function Banner() {
 
                 {/* Description */}
                 <h1 className="banner__description">
-                    {movie?.overview}
+                    {truncate(movie?.overview, 150)}
                 </h1>
+
+                <div className="banner__fadeBottom"></div>
 
             </div>
 
